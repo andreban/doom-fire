0b8015e0f4081422c0dc5fd2eb70272729bc5deb
Andre Bandarra <andreban@google.com>
2020-07-31 17:01:38 +0100

    Fullscreen / wake lock

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import './doom-fire.mjs';
 
-// if ('serviceWorker' in navigator) {
-//   // Use the window load event to keep the page load performant
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/sw.js');
-//   });
-// }
+const keepAwakeButton = document.querySelector('#keepAwake');
+const fullscreenButton = document.querySelector('#fullscreen');
+
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
+if ('wakeLock' in navigator) {
+  // Screen Wake Lock API supported 🎉
+  keepAwakeButton.removeAttribute('disabled');
+  keepAwakeButton.classList.remove('hidden');
+}
+
+keepAwakeButton.addEventListener('click', () => {});
+fullscreenButton.addEventListener('click', () => {});
