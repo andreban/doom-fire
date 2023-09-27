14c2e99a572f1817feeac5a4d65d175377e98baa
André Cipriani Bandarra <andreban@gmail.com>
2023-09-27 08:18:46 +0100

    Remove scroll bars from main window

--- public/sw.js
+++ public/sw.js
@@ -1255,7 +1255,7 @@
 
     // Use the imported Workbox libraries to implement caching,
     // routing, and other logic:
-    precacheAndRoute([{"revision":"95beace530ce617aa24db2884f8a49b7","url":"bundle.js"},{"revision":"132b6aa8334bbd822c8d10eea51c57a6","url":"doom-fire-worker.js"},{"revision":"0a27a4163254fc8fce870c8cc3a3f94f","url":"404.html"},{"revision":"ee29e937a6cc093e5686d57dd9645081","url":"index.html"}]);
+    precacheAndRoute([{"revision":"68dd18064edcd7309250f49697dec68a","url":"bundle.js"},{"revision":"2e77424852f486041ee0b9a0b5a4c21f","url":"doom-fire-worker.js"},{"revision":"0a27a4163254fc8fce870c8cc3a3f94f","url":"404.html"},{"revision":"10cfa7edf05226d20fb1cd446d9f7bd1","url":"index.html"}]);
 
 }());
 //# sourceMappingURL=sw.js.map
